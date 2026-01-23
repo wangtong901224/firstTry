a104bc32506b4386e282beef05f5f6476d64fe4b
wangtong901224 <89619863+wangtong901224@users.noreply.github.com>
2026-01-23 13:51:42 +0800

    Update QD.sh

--- .github/QD.sh
+++ .github/QD.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 sckey='SCT227039T75JQ4c5X0Qew9RSmwuoMh6Zf'
-cookie='koa:sess=eyJ1c2VySWQiOjQxNzE3NiwiX2V4cGlyZSI6MTcyMzM1NjgyMTY4MSwiX21heEFnZSI6MjU5MjAwMDAwMDB9; koa:sess.sig=lHysRFZ5bDml4MwnvmcCxjfh3q8; _gid=GA1.2.1535417043.1697703952; Cookie=enabled; Cookie.sig=lbtpENsrE0x6riM8PFTvoh9nepc; _ga=GA1.1.1033166729.1693981759; _ga_CZFVKMNT9J=GS1.1.1697703951.3.1.1697705517.0.0.0'
-referer='https://glados.one/console/checkin'
-origin='https://glados.one'
-useragent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36 Edg/98.0.1108.56'
+cookie='koa:sess=eyJ1c2VySWQiOjQxNzE3NiwiX2V4cGlyZSI6MTc5NDk4MzI4Mjk3NSwiX21heEFnZSI6MjU5MjAwMDAwMDB9; koa:sess.sig=uTVsTm5ovFUICswWr0jzh-fxUpY'
+referer='https://glados.cloud/api/user/checkin'
+origin='https://glados.cloud'
+useragent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
+authorization='12833298897138375142270069988788-864-1536'
+authority='glados.cloud'
 echo '------------------sign------------------'
-title=$(curl -H "cookie:$cookie" -H "referer:$referer" -H "origin:$origin" -H "user-agent:$useragent" -H 'content-type:application/json;charset=UTF-8' -d '{"token": "glados.one"}' -X POST 'https://glados.one/api/user/checkin' | grep -Eo '"message":"[^"]*"')
+title=$(curl -H "cookie:$cookie" -H "referer:$referer" -H "origin:$origin" -H "user-agent:$useragent" -H "authority:$authorization" -H 'content-type:application/json;charset=UTF-8' -d '{"token": "glados.cloud"}' -X POST 'https://glados.cloud/api/user/checkin' | grep -Eo '"message":"[^"]*"')
 echo $title
 title=$(echo ${title#*:} | sed 's/\"//g')
 echo '-----------------status-----------------'
-time=$(curl -H "cookie:$cookie" -H "referer:$referer" -H "origin:$origin" -H "user-agent:$useragent" -X GET 'https://glados.one/api/user/status' | grep -Eo '"leftDays":"[^"]*"')
+time=$(curl -H "cookie:$cookie" -H "referer:$referer" -H "origin:$origin" -H "user-agent:$useragent" -X GET 'https://glados.cloud/api/user/status' | grep -Eo '"leftDays":"[^"]*"')
 echo $time
 time=$(echo ${time#*:} | sed 's/\"//g')
 time=${time%%.*}
